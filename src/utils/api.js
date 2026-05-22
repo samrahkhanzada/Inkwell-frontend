e46f9b6cb066baadfd2eed6a0049218fba6ee292
Samrah Khanzada <samrahsajidkhanzada@gmail.com>
2026-05-22 18:51:48 +0500

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
+  // baseURL: process.env.REACT_APP_API_URL,
   headers: { 'Content-Type': 'application/json' },
 });
 
